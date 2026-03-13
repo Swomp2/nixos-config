4c8a9931d6cb6d93f4cffc9cc35b68f94bd1cbcd
Swomp2 <swomp226@gmail.com>
2026-03-13 16:07:17 +0300

    Небольшие изменения

--- home/vm.nix
+++ home/vm.nix
@@ -1,12 +1,12 @@
 {config, pkgs, ...}:
 {
-  xdg.configFile."hypr/hyprland-pc.conf".source = ../config/hypr/hyprland.conf;
-  xdg.configFile."hypr/hyprlock.conf".source = ../config/hypr/hyprlock.conf;
-  xdg.configFile."hypr/hypridle.conf".source = ../config/hypr/hypridle.conf;
+  xdg.configFile."hypr/hyprland-pc.conf".source = ./config/hypr/hyprland.conf;
+  xdg.configFile."hypr/hyprlock.conf".source = ./config/hypr/hyprlock.conf;
+  xdg.configFile."hypr/hypridle.conf".source = ./config/hypr/hypridle.conf;
 
-  xdg.configFile."waybar/config.jsonc".source = ../config/waybar/config.jsonc;
-  xdg.configFile."waybar/style.css".source = ../config/waybar/style.css;
+  xdg.configFile."waybar/config.jsonc".source = ./config/waybar/config.jsonc;
+  xdg.configFile."waybar/style.css".source = ./config/waybar/style.css;
 
-  xdg.configFile."kitty/kitty.conf".source = ../config/kitty/kitty.conf;
-  xdg.configFile."dunst/dunstrc".source = ../config/dunst/dunstrc;
+  xdg.configFile."kitty/kitty.conf".source = ./config/kitty/kitty.conf;
+  xdg.configFile."dunst/dunstrc".source = ./config/dunst/dunstrc;
 }
