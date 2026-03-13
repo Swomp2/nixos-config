4ee311349cb9b989324ca66f923339ff6e4352b7
Swomp2 <swomp226@gmail.com>
2026-03-14 00:22:13 +0300

    Небольшие изменения

--- home/config/bemenu/power.fish
+++ home/config/bemenu/power.fish
@@ -1,12 +1,11 @@
 #!/bin/fish
 
-set answer (echo "Выключиться"\n"Перезагрузиться"\n"Выйти"| bemenu --nb "#282828" --nf "#fbf1c7" --tb "#98971a" --tf "#282828" --fb "#282828" --ff "#fbf1c7" --hb "#d65d0e" --hf "#282828" --ab "#282828" --af "#fbf1c7" -H 10 -M 1130 -c --border 2 --bdr "#d65d0e" --prompt="Что сделать? " --list 5 -i --fn "Ubuntu Regular 12")
+set answer (echo "Выключиться"\n"Перезагрузиться"\n"Выйти"| bemenu --nb "#282828" --nf "#fbf1c7" --tb "#d65d0e" --tf "#282828" --fb "#282828" --ff "#fbf1c7" --hb "#d65d0e" --hf "#282828" --ab "#282828" --af "#fbf1c7" -H 10 -M 1130 -c --border 2 --bdr "#d65d0e" --prompt="Что сделать? " --list 5 -i --fn "Ubuntu Regular 12")
 
 if test $answer = "Перезагрузиться"
-   exec loginctl reboot
+   exec systemctl reboot
 else if test $answer = "Выключиться"
-   exec loginctl poweroff
+   exec systemctl poweroff
 else if test $answer = "Выйти"
-   pgrep -x sway || exec hyprctl dispatch exit
-   pgrep -x Hyprland || exec swaymsg exit
+   exec hyprctl dispatch exit
 end
