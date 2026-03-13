b123364e76d4733b374aecd60375dac8387ff2bc
Swomp2 <swomp226@gmail.com>
2026-03-13 18:26:02 +0300

    Небольшие изменения

--- home/config/bemenu/power.fish
+++ home/config/bemenu/power.fish
@@ -1,6 +1,6 @@
 #!/bin/fish
 
-set answer (echo "Выключиться"\n"Перезагрузиться"\n"Выйти"| bemenu --nb "#282828" --nf "#fbf1c7" --tb "#98971a" --tf "#282828" --fb "#282828" --ff "#fbf1c7" --hb "#98971a" --hf "#282828" --ab "#282828" --af "#fbf1c7" -H 10 -M 1130 -c --border 2 --bdr "#98971a" --prompt="Что сделать? " --list 5 -i --fn "Ubuntu Regular 10")
+set answer (echo "Выключиться"\n"Перезагрузиться"\n"Выйти"| bemenu --nb "#282828" --nf "#fbf1c7" --tb "#98971a" --tf "#282828" --fb "#282828" --ff "#fbf1c7" --hb "#d65d0e" --hf "#282828" --ab "#282828" --af "#fbf1c7" -H 10 -M 1130 -c --border 2 --bdr "#d65d0e" --prompt="Что сделать? " --list 5 -i --fn "Ubuntu Regular 12")
 
 if test $answer = "Перезагрузиться"
    exec loginctl reboot
