f880e3d48ed27a03afaa3225bb6b2284aa218c53
Swomp <currentswomp@gmail.com>
2026-05-03 11:46:47 +0300

    Исправлен синтаксис анимаций

--- home/config/hypr/winRules.nix
+++ home/config/hypr/winRules.nix
@@ -2,19 +2,19 @@
 {
   wayland.windowManager.hyprland = {
     settings = {
-      windowrulev2 = [
-        "float, title:^(Громкость)$"
-        "float, title:^(Копирование файлов)$"
-        "float, title:^(Подтвердить замену файлов)$"
-        "float, title:^(Настройки)$"
-        "float, title:^(Настройки — Strawberry Music Player)$"
-        "float, title:^(Эквалайзер — Strawberry Music Player)$"
-        "float, title:^(Менеджер обложек — Strawberry Music Player)$"
-        "float, title:^(Библиотека)$"
+      windowrule = [
+        "title:^(Громкость)$, float on"
+        "title:^(Копирование файлов)$, float on"
+        "title:^(Подтвердить замену файлов)$, float on"
+        "title:^(Настройки)$, float on"
+        "title:^(Настройки — Strawberry Music Player)$, float on"
+        "title:^(Эквалайзер — Strawberry Music Player)$, float on"
+        "title:^(Менеджер обложек — Strawberry Music Player)$, float on"
+        "title:^(Библиотека)$, float on"
 
-        "float, class:^(lxqt-policykit-agent)$"
-        "workspace 2, class:^(org.strawberrymusicplayer.strawberry)$"
-        "float, class:^(org.kde.ark)$"
+        "class:^(lxqt-policykit-agent)$, float on"
+        "class:^(org.strawberrymusicplayer.strawberry)$, workspace 2"
+        "class:^(org.kde.ark)$ float on"
       ];
     };
   };
