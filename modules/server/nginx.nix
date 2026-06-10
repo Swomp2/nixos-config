c574d7d0127b28cc8de919fb1bbd8db5d3d5e784
Swomp2 <currentswomp@gmail.com>
2026-06-10 18:42:44 +0300

    Исправлены небольшие ошибки в файлах

--- modules/server/nginx.nix
+++ modules/server/nginx.nix
@@ -194,10 +194,6 @@ in
           proxyWebsockets = true;
 
           extraConfig = ''
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-
             proxy_read_timeout 3600;
             proxy_send_timeout 3600;
           '';
